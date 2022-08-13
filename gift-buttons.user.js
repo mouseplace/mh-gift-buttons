39522fd49ba8443975b5ff779c0397d5750c0809
Brad Parbs <brad@bradparbs.com>
2022-08-13 11:49:44 -0500

    Fix version

--- gift-buttons.user.js
+++ gift-buttons.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         🐭️ MouseHunt - Gift Buttons
-// @version      1.0.1
+// @version      1.0.0
 // @description  Add buttons to easily accept and return all daily gifts.
 // @license      MIT
 // @author       bradp
